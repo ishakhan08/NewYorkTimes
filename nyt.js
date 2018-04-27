0a073272a9a81f85c4e7eb696cee96b4e89ff30a
Michael Dillie <michaeldillie@gmail.com>
2018-04-27 14:03:56 -0500

    Can now get data from API when searching topic, start year, and end year

--- nyt.js
+++ nyt.js
@@ -1,51 +1,36 @@
 $(document).ready(function () {
     //*** This is a test ***/
+    var searchBtn = $("#submit");
+    var clearBtn = $("#clear");
 
+    $("#submit").on("click", function (event) {
+        event.preventDefault();
 
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    url += '?' + $.param({
-        'api-key': "3b45ac8d4ca74e1aad213cc2a28cd93d",
-        // trump is the search variable
-        "q": "trump",
-        // 20170101 is the start date variable
-        "begin_date": "20170101",
-        // 20180101 is the end date variable
-        "end_date": "20180101"
+        var term = $("#searchInput").val();
+        var startDate = $("#startYearInput").val().trim();
+        var endDate = $("#endYearInput").val().trim();
+
+        var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        url += '?' + $.param({
+            'api-key': "3b45ac8d4ca74e1aad213cc2a28cd93d",
+            // trump is the search variable
+            "q": term,
+            // 20170101 is the start date variable
+            "begin_date": startDate + "0101",
+            // 20180101 is the end date variable
+            "end_date": endDate + "0101"
+        });
+        $.ajax({
+            url: url,
+            method: 'GET',
+        }).done(function (result) {
+            console.log(result.response.docs);
+        }).fail(function (err) {
+            throw err;
+        });
+
+        console.log(url);
+
     });
-    $.ajax({
-        url: url,
-        method: 'GET',
-    }).done(function (response) {
-        console.log(response);
-    }).fail(function (err) {
-        throw err;
-    });
-
-
-    queryp = $("#search-term")
-        .val()
-        .trim();
-
-    queryStart = $("#start-year")
-        .val()
-        .trim();
-
-    queryEnd = $("#end-year")
-        .val()
-        .trim();
-
-
-
-
-    startYear = $("#start-year" + 0101);
-    endYear = $("#end-year" + 0101);
-
-    paragraph = $("<p>");
-    div = $("<div>");
-
-
-    $("#articles").append(div);
-
-
 
 });
