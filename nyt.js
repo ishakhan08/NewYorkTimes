a69c390fe253ec11c127bf61c027fdf8adf1aef2
Isha Khan <khan.isha08@gmail.com>
2018-04-27 10:54:58 -0500

    changes in index.html

---
+++ nyt.js
@@ -0,0 +1,55 @@
+$( document ).ready(function() {
+
+
+
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+      'api-key': "3b45ac8d4ca74e1aad213cc2a28cd93d"
+    });
+    $.ajax({
+      url: url,
+      method: 'GET',
+    }).done(function(result) {
+      console.log(result);
+    }).fail(function(err) {
+      throw err;
+    });
+  
+  
+  queryp = $("#search-term")
+  .val()
+  .trim();
+  
+  queryStart = $("#start-year")
+  .val()
+  .trim();
+  
+  queryEnd = $("#end-year")
+  .val()
+  .trim();
+  
+  
+  
+  
+  startYear = $("#start-year" + 0101);
+  endYear = $("#end-year" + 0101);
+  
+  paragraph = $("<p>");
+  div = $("<div>");
+  
+  
+  $("#articles").append(div);
+  
+  
+   
+  });
+  
+  
+
+
+
+
+
+
+
+  
